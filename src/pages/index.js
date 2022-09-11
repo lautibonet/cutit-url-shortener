d06fa500aa932865696b1a132ff9f444ec195fb9
lautibonet <lautibonet@gmail.com>
2022-09-10 21:35:24 -0300

    - Added call to api from button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Flex, Heading, Input, Button } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function Home() {
+  const [longURL, setLongURL] = useState('')
+
+  const handleClick = () => {
+    if (!longURL) return
+    fetch('/api/url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ original: longURL }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }
+
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background="gray.100" p={12} rounded={6}>
@@ -10,12 +26,16 @@ export default function Home() {
           Minify your long URL
         </Heading>
         <Input
+          value={longURL}
+          onChange={(e) => setLongURL(e.target.value)}
           placeholder="Looong URL"
           variant="outline"
           mb={8}
           type="text"
         ></Input>
-        <Button colorScheme="blue">Make it Mini!</Button>
+        <Button colorScheme="blue" onClick={handleClick}>
+          Make it Mini!
+        </Button>
       </Flex>
     </Flex>
   )
