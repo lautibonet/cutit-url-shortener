58df49132ae16880997b3877fc84bc841e60b88a
lautibonet <lautibonet@gmail.com>
2022-09-13 15:56:13 -0300

    - Added head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Mini URL</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
